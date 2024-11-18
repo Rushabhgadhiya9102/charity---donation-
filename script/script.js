6572efbb3b8212d36a86552734426201a34e8154
Rushabhgadhiya9102 <rushabhgadhiya9102@gmail.com>
2024-11-19 04:05:25 +0530

    done

--- script/script.js
+++ script/script.js
@@ -1,14 +1,20 @@
 document.addEventListener("DOMContentLoaded",function (){
 
-    const navigation = document.querySelector('.navbar .container');
+    const navigation = document.querySelector('.navbar .container-fluid');
+    const navBrand = document.querySelector(".bi-heart-pulse-fill");
+    const login = document.querySelector(".bi-person-circle");
 
     window.addEventListener('scroll',() => {
         if(window.scrollY > 0){
             navigation.classList.add('scrolled');
-                
+            navBrand.style.display="none";
+            login.style.display="none";
         }else{
-            navigation.classList.remove('scrolled');    
+            navigation.classList.remove('scrolled'); 
+            navBrand.style.display="block";   
+            login.style.display="block";   
         }
     });
+
 
 });
